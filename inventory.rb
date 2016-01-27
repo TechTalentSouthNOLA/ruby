b778d5afa679a148dd7de52e30661d5d229720fb
Sia <siakaramalegos@gmail.com>
2016-01-27 08:39:52 -0600

    Adds rename method and explains to_sym

--- inventory.rb
+++ inventory.rb
@@ -14,6 +14,8 @@ class Inventory
         show_items
       when 2
         change_item
+      when 3
+        rename_item
       else
         break
       end
@@ -29,6 +31,7 @@ class Inventory
     puts "\nWhat do you want to do?"
     puts " 1 = show all items"
     puts " 2 = change an item"
+    puts " 3 = rename an item"
     puts " 9 = quit"
     gets.to_i
   end
@@ -45,6 +48,7 @@ class Inventory
     show_items
 
     print " enter item > "
+    # to_sym converts the string into a symbol
     gets.chomp.to_sym
   end
 
@@ -54,6 +58,16 @@ class Inventory
       puts "  #{item}:  #{value}"
     end
     puts "*" * 40 + "\n\n"
+  end
+
+  def rename_item
+    puts "Which item do you want to rename?"
+    item = get_item
+
+    puts "What would you like to rename #{item} to?"
+    new_name = gets.chomp.to_sym
+
+    @inventory[new_name] = @inventory.delete(item)
   end
 end
 
